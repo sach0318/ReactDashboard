f8ee18cf3432f79bcb21f306c6bf3e09db1ff627
sach0318 <sachinyadv0318@gmail.com>
2025-05-27 20:29:43 +0530

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import OverviewPage from "./Pages/OverviewPage";
+import ProductsPage from "./Pages/ProductsPage";
+import Sidebar from "./components/Sidebar";
+
 function App() {
   return (
-    <>
-      <div>
-        <h1>hello</h1>
+    <div className=" flex h-screen bg-gray-900 text-gray-100 overflow-hidden    ">
+      <div className=" fixed inset-0 z-0 ">
+        <div className=" absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80 " />
+        <div className=" absolute inset-0 backdrop-blur-sm   "   />
       </div>
-    </>
+
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<OverviewPage />} />
+        <Route path="/Products" element={<ProductsPage />} />
+        <Route path="/" element={<OverviewPage />} />
+        <Route path="/" element={<OverviewPage />} />
+      </Routes>
+    </div>
   );
 }
 
